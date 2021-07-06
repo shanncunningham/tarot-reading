c4747f1de30f617e7ea0f490991d7430ac44336a
shanncunningham <shanncunningham@users.noreply.github.com>
2021-07-06 10:58:08 -0400

    add cards object dictionary

---
+++ objects.js
@@ -0,0 +1,330 @@
+const cards = {
+    theFool: {
+        name: 'The Fool',
+        description: 'A fool'
+    },
+    theMagician: {
+        name: 'The Magician',
+        description: 'A magician'
+    },
+    theHighPriestess: {
+        name: 'The High Priestess',
+        description: 'the highest'
+    },
+    theEmpress: {
+        name: 'The Empress',
+        description: 'Description'
+    },
+    theEmperor: {
+        name: 'The Emperor',
+        description: 'description'
+    },
+    theHeirophant: {
+        name: 'The Heirophant',
+        description: 'description'
+    },
+    theLovers: {
+        name: 'The Lovers',
+        description: 'description'
+    },
+    theChariot: {
+        name: 'The Chariot',
+        description: 'description'
+    },
+    strength: {
+        name: 'Strength',
+        description: 'description'
+    },
+    theHermit: {
+        name: 'The Hermit',
+        description: 'description'
+    },
+    theWheelOfFortune: {
+        name: 'The Wheel of fortune',
+        description: 'hi'
+    },
+    justice: {
+        name: 'Justice',
+        description: 'desc'
+    },
+    theHangedMan: {
+        name: 'The Hanged Man',
+        description: 'desc'
+    },
+    death: {
+        name: 'Death',
+        description: 'desc'
+    },
+    temperance: { 
+        name: 'Temperance',
+        description: 'desc'
+    },
+    theDevil: {
+        name: 'The Devil',
+        description: 'desc'
+    },
+    theTower: {
+        name: 'The Tower',
+        description: 'desc'
+    },
+    theStar: {
+        name: 'The Star',
+        description: 'desc'
+    },
+    theMoon: {
+        name: 'The Moon',
+        description: 'desc'
+    },
+    theSun: {
+        name: 'The Sun',
+        description: 'desc'
+    },
+    judgement: {
+        name: 'Judgement',
+        description: 'desc'
+    },
+    theWorld: {
+        name: 'The World',
+        description: 'desc'
+    },
+    aceWands: {
+        name: 'Ace of Wands',
+        description: ''
+    },
+    aceCups: {
+        name: 'Ace of Cups',
+        description: ''
+    },
+    aceSwords: {
+        name: 'Ace of Swords',
+        description: ''
+    },
+    acePentacles: {
+        name: 'Ace of Pentacles',
+        description: ''
+    },
+    twoWands: {
+        name: 'Two of Wands',
+        description: ''
+    },
+    twoCups: {
+        name: 'Two of Cups',
+        description: ''
+    },
+    twoSwords: {
+        name: 'Two of Swords',
+        description: ''
+    },
+    twoPentacles: {
+        name: 'Two of Pentacles',
+        description: ''
+    },
+    threeWands: {
+        name: 'Three of Wands',
+        description: ''
+    },
+    threeCups: {
+        name: 'Three of Cups',
+        description: ''
+    },
+    threeSwords: {
+        name: 'Three of Swords',
+        description: ''
+    },
+    threePentacles: {
+        name: 'Three of Pentacles',
+        description: ''
+    },
+    fourWands: {
+        name: 'Four of Wands',
+        description: ''
+    },
+    fourCups: {
+        name: 'Four of Cups',
+        description: ''
+    },
+    fourSwords: {
+        name: 'Four of Swords',
+        description: ''
+    },
+    fourPentacles: {
+        name: 'Four of Pentacles',
+        description: ''
+    },
+    fiveWands: {
+        name: 'Five of Wands',
+        description: ''
+    },
+    fiveCups: {
+        name: 'Five of Cups',
+        description: ''
+    },
+    fiveSwords: {
+        name: 'Five of Swords',
+        description: ''
+    },
+    fivePentacles: {
+        name: 'Five of Pentacles',
+        description: ''
+    },
+    sixWands: {
+        name: 'Six of Wands',
+        description: ''
+    },
+    sixCups: {
+        name: 'Six of Cups',
+        description: ''
+    },
+    sixSwords: {
+        name: 'Six of Swords',
+        description: ''
+    },
+    sixPentacles: {
+        name: 'Six of Pentacles',
+        description: ''
+    },
+    sevenWands: {
+        name: 'Seven of Wands',
+        description: ''
+    },
+    sevenCups: {
+        name: 'Seven of Cups',
+        description: ''
+    },
+    sevenSwords: {
+        name: 'Seven of Swords',
+        description: ''
+    },
+    sevenPentacles: {
+        name: 'Seven of Pentacles',
+        description: ''
+    },
+    eightWands: {
+        name: 'Eight of Wands',
+        description: ''
+    },
+    eightCups: {
+        name: 'Eight of Cups',
+        description: ''
+    },
+    eightSwords: {
+        name: 'Eight of Swords',
+        description: ''
+    },
+    eightPentacles: {
+        name: 'Eight of Pentacles',
+        description: ''
+    },
+    nineWands: {
+        name: 'Nine of Wands',
+        description: ''
+    },
+    nineCups: {
+        name: 'Nine of Cups',
+        description: ''
+    },
+    nineSwords: {
+        name: 'Nine of Swords',
+        description: ''
+    },
+    ninePentacles: {
+        name: 'Nine of Pentacles',
+        description: ''
+    },
+    tenWands: {
+        name: 'Ten of Wands',
+        description: ''
+    },
+    tenCups: {
+        name: 'Ten of Cups',
+        description: ''
+    },
+    tenSwords: {
+        name: 'Ten of Swords',
+        description: ''
+    },
+    tenPentacles: {
+        name: 'Ten of Pentacles',
+        description: ''
+    },
+    pageWands: {
+        name: 'Page of Wands',
+        description: ''
+    },
+    pageCups: {
+        name: 'Page of Cups',
+        description: ''
+    },
+    pageSwords: {
+        name: 'Page of Swords',
+        description: ''
+    },
+    pagePentacles: {
+        name: 'Page of Pentacles',
+        description: ''
+    },
+    knightWands: {
+        name: 'Knight of Wands',
+        description: ''
+    },
+    knightCups: {
+        name: 'Knight of Cups',
+        description: ''
+    },
+    knightSwords: {
+        name: 'Knight of Swords',
+        description: ''
+    },
+    knightPentacles: {
+        name: 'Knight of Pentacles',
+        description: ''
+    },
+    queenWands: {
+        name: 'Queen of Wands',
+        description: ''
+    },
+    queenCups: {
+        name: 'Queen of Cups',
+        description: ''
+    },
+    queenSwords: {
+        name: 'Queen of Swords',
+        description: ''
+    },
+    queenPentacles: {
+        name: 'Queen of Pentacles',
+        description: ''
+    },
+    kingWands: {
+        name: 'King of Wands',
+        description: ''
+    },
+    kingCups: {
+        name: 'King of Cups',
+        description: ''
+    },
+    kingSwords: {
+        name: 'King of Swords',
+        description: ''
+    },
+    kingPentacles: {
+        name: 'King of Pentacles',
+        description: ''
+    },
+    knightWands: {
+        name: 'Knight of Wands',
+        description: ''
+    },
+    knightCups: {
+        name: 'Knight of Cups',
+        description: ''
+    },
+    knightSwords: {
+        name: 'Knight of Swords',
+        description: ''
+    },
+    knightPentacles: {
+        name: 'Knight of Pentacles',
+        description: ''
+    }
+  };
